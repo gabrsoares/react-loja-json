7cccc789262f0d5dd18cff73eac1169851e73f5a
Gabriel Soares <gabrsoares@outlook.com>
2023-10-09 17:42:58 -0300

    finish banner & slider categories

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
 import Header from './Components/header/Header';
+import OffBanner from './Components/offBaner/OffBanner';
+import SlickerCategories from './Components/slickerCategories/SlickerCategories';
+import SlickerSlides from './Components/slickerSlides/SlickerSlides';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <SlickerSlides />
+      <OffBanner />
+      <SlickerCategories />
     </div>
   );
 }
