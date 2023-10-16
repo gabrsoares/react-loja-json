1624593f671ff7a528aa0f2014bbaecfdcacc719
Gabriel Soares <gabrsoares@outlook.com>
2023-10-16 17:00:30 -0300

    update readme, update products component, add json-server database & code to run

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './Components/header/Header';
 import OffBanner from './Components/offBaner/OffBanner';
@@ -6,13 +7,33 @@ import SlickerCategories from './Components/slickerCategories/SlickerCategories'
 import SlickerSlides from './Components/slickerSlides/SlickerSlides';
 
 function App() {
+  const [error, setError] = useState(null);
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3001/products")
+    .then(res => res.json())
+    .then((result) => {
+      setIsLoaded(true);
+      setItems(result);
+    }, (error) => {
+      setIsLoaded(true);
+      setError(error);
+    })
+  }, [])
+
+  const test = () => {
+    console.log(items)
+  }
+
   return (
     <div className="App">
       <Header />
       <SlickerSlides />
       <OffBanner />
       <SlickerCategories />
-      <ProductsPopular />
+      <ProductsPopular items={items} />
     </div>
   );
 }
--- src/Components/productsPopular/ProductsPopular.js
+++ src/Components/productsPopular/ProductsPopular.js
@@ -2,57 +2,27 @@ import { Button } from '../../ds'
 import './styles.css'
 import React from 'react'
 
-export default function ProductsPopular() {
+export default function ProductsPopular({items}) {
   return (
     <div className='products-popular flex'>
         <h2 className='products-title'>Mais vistos</h2>
         <div className='product-cards flex'>
-            <div className='product-card01 flex'>
-                <img src='https://static.zattini.com.br/produtos/tenis-vizzano-casual-feminino/14/E11-0730-014/E11-0730-014_zoom1.jpg?ts=1695700341&ims=326x' alt='Tênis Vizzano Casual Feminino'></img>
-                <span className='product-discount'>-5% OFF</span>
-                <p className='product-card-title'>Tênis Vizzano Casual Feminino</p>
-                <div className='product-card-text flex'>
-                    <span className='product-pretext'>A partir de</span>
-                    <p className='product-price'>R$ 74,99</p>
-                    <p>ou 1x de R$ 74,99</p>
-                </div>  
-                <Button align='center' variation='primary'>Comprar</Button>   
-            </div>
-            <div className='product-card01 flex'>
-                <img src='https://static.zattini.com.br/produtos/slip-on-santa-lolla-logo-feminino/06/H08-2245-006/H08-2245-006_zoom1.jpg?ts=1695700660&ims=326x' alt='Slip On Santa Lolla Logo Feminino'></img>
-                <span className='product-discount'>-15% OFF</span>
-                <p className='product-card-title'>Slip On Santa Lolla Logo Feminino</p>
-                <div className='product-card-text flex'>
-                    <span className='product-pretext strike'>R$ 129,90</span>
-                    <p className='product-price'>R$ 89,99</p>
-                    <p>ou 1x de R$ 89,99</p>
+            {items.map(item => (
+                <div className='product-card01 flex' key={item.id}>
+                    <img src={item.image} alt={item.alt}></img>
+                    {item.discount && <span className='product-discount'>{item.discount_amount}</span>}
+                    <p className='product-card-title'>{item.title}</p>
+                    <div className='product-card-text flex'>
+                        {item.tax && <p className='shipping'>Frete grátis</p>}
+                        {item.tax && <span className='product-pretext'>A partir de</span>}
+                        {item.discount && <span className='product-pretext strike'>{item.old_price}</span>}
+                        <p className='product-price'>{item.price}</p>
+                        <p>ou 1x de {item.price}</p>
+                        <Button align='center' variation='primary'>Comprar</Button>   
+                    </div>  
                 </div>
-                <Button align='center' variation='primary'>Comprar</Button>           
-            </div>
-            <div className='product-card01 flex'>
-                <img src='https://static.zattini.com.br/produtos/tenis-vizzano-casual-feminino/14/2DD-7206-014/2DD-7206-014_zoom1.jpg?ts=1695699181&ims=326x' alt='Tênis Vizzano Casual Feminino'></img>
-                <p className='product-card-title'>Tênis Vizzano Casual Feminino</p>
-                <div className='product-card-text flex'>
-                    <p className='shipping'>Frete grátis</p>
-                    <span className='product-pretext'>A partir de</span>
-                    <p className='product-price'>R$ 116,84</p>
-                    <p>ou 1x de R$ 116,84 </p>
-                </div>
-                <Button align='center' variation='primary'>Comprar</Button>   
-            </div>
-            <div className='product-card01 flex'>
-                <img src='https://static.zattini.com.br/produtos/scarpin-vizzano-salto-medio-bico-fino/06/E11-7358-006/E11-7358-006_zoom1.jpg?ts=1695699091&ims=326x' alt='Scarpin Vizzano Salto Médio Bico Fino'></img>
-                <span className='product-discount'>-22% OFF</span>
-                <p className='product-card-title'>Scarpin Vizzano Salto Médio Bico Fino</p>
-                <div className='product-card-text flex'>
-                    <span className='product-pretext strike'>R$ 129,99</span>
-                    <p className='product-price'>R$ 79,99</p><p>no Pix</p>
-                    <p>ou 1x de R$ 79,99</p>
-                </div>
-                <Button align='center' variation='primary'>Comprar</Button>   
-            </div>
+            ))}
         </div>
-        
     </div>
   )
 }
